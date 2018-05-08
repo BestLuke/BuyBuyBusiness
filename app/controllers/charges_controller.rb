f2f4808d18bde8a3abdeac5e4e6deabfc661d1c5
mrmimosizer <m1mosizer@gmail.com>
2018-05-08 11:33:49 +1000

    pulling over

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,10 +1,11 @@
 class ChargesController < ApplicationController
+    before_action :set_item
     def new
     end
     
     def create
       # Amount in cents
-      @amount = 500
+      @amount = @business.price*100
     
       customer = Stripe::Customer.create(
         :email => params[:stripeEmail],
@@ -15,11 +16,15 @@ class ChargesController < ApplicationController
         :customer    => customer.id,
         :amount      => @amount,
         :description => 'Rails Stripe customer',
-        :currency    => 'usd'
+        :currency    => 'aud'
       )
     
     rescue Stripe::CardError => e
       flash[:error] = e.message
       redirect_to new_charge_path
     end
+    private
+                  def set_item
+                    @business = Business.find(params[:business_id])
+                  end
 end
--- db/migrate/20180508013103_add_user_id_to_businesses.rb
+++ db/migrate/20180508013103_add_user_id_to_businesses.rb
@@ -1,4 +1,4 @@
-class AddUserToBusinesses < ActiveRecord::Migration[5.2]
+class AddUserIdToBusinesses < ActiveRecord::Migration[5.2]
   def change
     add_reference :businesses, :user, foreign_key: true
   end
