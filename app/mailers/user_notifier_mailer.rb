b12474b407349862b44ad4db9c6a17e1d922e201
mrmimosizer <m1mosizer@gmail.com>
2018-05-09 17:38:00 +1000

    heroku fix

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,5 +1,5 @@
 class UserNotifierMailer < ApplicationMailer
-    default :from => 'admin@ontherecord.com'
+    default :from => 'admin@buybuybusiness.com'
   
     # send a signup email to the user, pass in the user object that   contains the user's email address
     def send_signup_email(recipient)
