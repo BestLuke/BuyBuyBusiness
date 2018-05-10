b243f8190651e36c2cbc72de0474b467fcda269b
mrmimosizer <m1mosizer@gmail.com>
2018-05-10 10:42:48 +1000

    here

--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -17,7 +17,7 @@ if Rails.env.development?
     ActionMailer::Base.smtp_settings = {
       address:              'smtp.sendgrid.net',
       port:                 587,
-      domain:               'buy-buy-business.herokuapp.com/businesses.com',
+      domain:               'buy-buy-business.herokuapp.com',
       user_name:            ENV["SENDGRID_USERNAME"],
       password:             ENV["SENDGRID_PASSWORD"],
       authentication:       'plain',
